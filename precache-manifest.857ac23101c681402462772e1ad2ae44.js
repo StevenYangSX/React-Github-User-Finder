1c98200063c499f0731e93774708300737a6fa5d
Shixin Yang <shixiny@sfu.ca>
2019-10-30 14:06:06 -0700

    Updates

---
+++ precache-manifest.857ac23101c681402462772e1ad2ae44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29868163fbfd65a8df792b4688ac934b",
+    "url": "/React-Github-User-Finder/index.html"
+  },
+  {
+    "revision": "23fb2c04ee9eb10ae2d8",
+    "url": "/React-Github-User-Finder/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "a565346feb7dcc1bdf16",
+    "url": "/React-Github-User-Finder/static/js/2.ec4d95dc.chunk.js"
+  },
+  {
+    "revision": "23fb2c04ee9eb10ae2d8",
+    "url": "/React-Github-User-Finder/static/js/main.972e34c2.chunk.js"
+  },
+  {
+    "revision": "3b6a2907886cc64a75d2",
+    "url": "/React-Github-User-Finder/static/js/runtime-main.7de76a95.js"
+  }
+]);
