d5514493395f675c95706e8ac17b84d1a38006a0
StevenYangSX <yangshixin1206@icloud.com>
2020-01-02 18:30:28 -0800

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React, {Fragment} from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import User from './components/users/User';
-import Search from './components/users/Search';
-import Alert from './components/layout/Alert';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import About from './components/pages/About'
-import GithubState from './context/github/GithubState';
-import AlertState from './context/alert/AlertState';
-
+import React, { Fragment } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import User from "./components/users/User";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./components/pages/About";
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
 
 const App = () => {
-
   // async componentDidMount() {
   //   this.setState({loading: true});
   //   console.log(process.env.REACT_APP_GITHUB_CILENT_SECRET)
@@ -25,30 +23,30 @@ const App = () => {
       <AlertState>
         <Router>
           <div className="App">
-            <Navbar title="Github Finder" icon="fab fa-github"/> 
-              <div className="container">
-                <Alert />
-                <Switch>
-                  <Route exact path="/" render={props => (
+            <Navbar title="Github Finder" icon="fab fa-github" />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={props => (
                     <Fragment>
                       <Search />
                       <Users />
                     </Fragment>
-                  )}/>
-                  <Route exact path="/about" component={About}/>
-                  
-                  <Route exact path="/user/:login" component={User}/>
-                  )}/>
-                </Switch>
-                
-              </div>      
+                  )}
+                />
+                <Route exact path="/about" component={About} />
+
+                <Route exact path="/user/:login" component={User} />
+              </Switch>
+            </div>
           </div>
         </Router>
       </AlertState>
     </GithubState>
   );
-
- 
-}
+};
 
 export default App;
